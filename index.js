bd99fc8d489fe2a8c68c720236bef583f62f67ea
Kartik Jain <70012621+KartikJain14@users.noreply.github.com>
2024-02-15 16:21:41 +0530

    Revert to Jan 2
    https://github.com/KartikJain14/notepad/commit/cf2994b557c9f00b184ec96817f6aa54133925b7
    Reason: Bugs in saving file.

--- index.js
+++ index.js
@@ -10,7 +10,58 @@ const loadButton = document.getElementById("load-button");
 const clearButton = document.getElementById("clear-button");
 const clearConfirmButton = document.getElementById("clear-confirm-button")
 const editor = document.getElementById("editor");
+const key = "PrEzTyL!";
 
+@@ -28,6 +29,11 @@ function exit(){
+}
+
+function clear(){
+    clearButton.hidden = true;
+    clearConfirmButton.hidden = false;
+}
+
+function confirmClear(){
+    localStorage.removeItem("dataText");
+    editor.innerHTML='\n        Start typing here...\n    ';
+    exit();
+@@ -62,6 +68,7 @@ strikethroughButton.addEventListener("click", () => formatText("strikethrough"))
+saveButton.addEventListener("click", () => save());
+loadButton.addEventListener("click", () => triggerFileInput());
+clearButton.addEventListener("click", () => clear());
+clearConfirmButton.addEventListener("click", () => confirmClear());
+
+colorPicker.addEventListener("input", function () {
+    const color = colorPicker.value;
+    document.execCommand("styleWithCSS", false, true);
+    document.execCommand("foreColor", false, color);
+    document.execCommand("styleWithCSS", false, false);
+});
+increaseFontSizeButton.addEventListener("click", () => changeLineFontSize(1.5));
+decreaseFontSizeButton.addEventListener("click", () => changeLineFontSize(1));
+function save(){
+    const content = document.getElementById('editor').innerHTML;
+    const enc = encrypt(content, key);
+    localStorage.setItem("dataText",enc);
+    const link = document.createElement("a");
+    const file = new Blob([enc], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = "note.pxt";
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+function saved() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const content = e.target.result;
+        const dec = decrypt(content, key);
+        const editor = document.getElementById('editor');
+        editor.innerHTML = dec;
+        fileInput.value = "";
+    };
+    reader.readAsText(file);
+}
 function encrypt(text, key){
     return [...text].map((x, i) => 
     (x.codePointAt() ^ key.charCodeAt(i % key.length) % 255)
@@ -24,125 +75,12 @@ function decrypt(text, key){
        parseInt(e, 16) ^ key.charCodeAt(i % key.length) % 255)
      )
 }
-
-function load(){
-    const key = "PrEzTyL!";
-    console.log("If you know why you are here, contact admin@preztyl.tech");
-    try{
-        const encDataText = localStorage.getItem("dataText");
-        const decDataText = decrypt(encDataText, key);
-        if(encDataText !== ''){
-            console.log("Loaded previous session data!");
-            decDataText = '\n        Start typing here...\n    ';
-            editor.innerHTML = decDataText;
-        }catch(e){
-        console.log("No previous session data found!");
-    }
-}
-
-function exit(){
-    const key = "PrEzTyL!";
-    try{
-        const textData = editor.innerHTML;
-        const encTextData = encrypt(textData, key);
-        localStorage.setItem("dataText", encTextData)
-    }catch(e){
-        console.log("Failed to save data in local storage");
-    }
-}
-
-function clear(){
-    clearButton.hidden = true;
-    clearConfirmButton.hidden = false;
-}
-
-function confirmClear(){
-    localStorage.removeItem("dataText");
-    editor.innerHTML='\n        Start typing here...\n    ';
-    exit();
-    location.reload();
-}
-
-const intervalId = setInterval(exit, 1000);
-
-window.onbeforeunload = confirmExit;
-function confirmExit() {
-    exit();
-}
-
-function formatText(command) {
-    document.execCommand(command, false, null);
-}
-
-function changeLineFontSize(size) {
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const startNode = range.startContainer;
-    if (startNode.nodeType === Node.TEXT_NODE) {
-        const line = startNode.parentNode;
-        line.style.fontSize = size + "rem";
-    }
-}
-
-boldButton.addEventListener("click", () => formatText("bold"));
-italicButton.addEventListener("click", () => formatText("italic"));
-underlineButton.addEventListener("click", () => formatText("underline"));
-strikethroughButton.addEventListener("click", () => formatText("strikethrough"));
-saveButton.addEventListener("click", () => save());
-loadButton.addEventListener("click", () => triggerFileInput());
-clearButton.addEventListener("click", () => clear());
-clearConfirmButton.addEventListener("click", () => confirmClear());
-
-colorPicker.addEventListener("input", function () {
-    const color = colorPicker.value;
-    document.execCommand("styleWithCSS", false, true);
-    document.execCommand("foreColor", false, color);
-    document.execCommand("styleWithCSS", false, false);
-});
-
-increaseFontSizeButton.addEventListener("click", () => changeLineFontSize(1.5));
-decreaseFontSizeButton.addEventListener("click", () => changeLineFontSize(1));
-
-function save(){
-    const key = "PrEzTyL!";
-    try{
-        const content = document.getElementById('editor').innerHTML;
-        const enc = encrypt(content, key);
-        localStorage.setItem("dataText",enc);
-    } catch(e){
-        console.log("Failed to save the data in localstorage.");
-    }
-    const link = document.createElement("a");
-    const file = new Blob([enc], { type: 'text/plain' });
-    link.href = URL.createObjectURL(file);
-    link.download = "note.pxt";
-    link.click();
-    URL.revokeObjectURL(link.href);
-}
-
-function saved() {
-    const key = "PrEzTyL!";
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const content = e.target.result;
-        const dec = decrypt(content, key);
-        const editor = document.getElementById('editor');
-        editor.innerHTML = dec;
-        fileInput.value = "";
-    };
-    reader.readAsText(file);
-}
-
 function triggerFileInput() {
     document.getElementById('fileInput').click();
 }
-
 function handleFileSelection(event) {
   const fileInput = event.target;
   const selectedFile = fileInput.files[0];
-
   if (selectedFile && selectedFile.name.endsWith('.pxt')) {
     saved(selectedFile);
   } else {
@@ -150,14 +88,12 @@ function handleFileSelection(event) {
   }
   fileInput.value = '';
 }
-
 function removeHolder(){
     content = document.getElementById('editor').innerHTML;
     if(content.startsWith('\n        Start typing here...\n    ')){
         document.getElementById('editor').innerHTML = "";
     }
 }
-
 document.addEventListener('keydown', function(event) {
     if (event.ctrlKey && event.key === 's') {
       event.preventDefault();
